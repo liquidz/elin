820c3fbc11eae4ee88f7192e0f2f1409c80a90ea
liquidz <liquidz.uo@gmail.com>
2024-01-15 17:54:13 +0900

    chore: Remove extra log

--- src/elin/component/handler.clj
+++ src/elin/component/handler.clj
@@ -13,15 +13,12 @@
   [req-map]
   (let [{:as arg :keys [async?]} (merge req-map
                                         (e.p.rpc/parse-request req-map))]
-    ;; (e.log/info "handler arg" (pr-str arg))
     (if-not async?
       (e.h.core/handler* arg)
       (future
         (let [resp (e.h.core/handler* arg)
               {:keys [callback]} arg]
           (try
-            (e.log/info "FIXME try to callback" (pr-str resp))
-            ;; (e.p.rpc/notify! req-map "nvim_call_function" ["elin#callback#call" [callback resp]])
             (e.p.rpc/call-function req-map "elin#callback#call" [callback resp])
             (catch Exception ex
               (e.log/log (str "FIXME callback error"
