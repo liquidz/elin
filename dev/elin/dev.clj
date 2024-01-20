a96fd2bed9cd19c33f61bcc2a27d3dded688a88c
liquidz <liquidz.uo@gmail.com>
2024-01-21 00:07:53 +0900

    fix: Fix dev server to be able to capture last message

--- dev/elin/dev.clj
+++ dev/elin/dev.clj
@@ -3,14 +3,23 @@
    [clojure.edn :as edn]
    [clojure.java.io :as io]
    [com.stuartsierra.component :as component]
+   [elin.constant.kind :as e.c.kind]
    [elin.log :as e.log]
    [elin.system :as e.system]
    [malli.dev :as m.dev]))
 
+(def ^:private last-message-store (atom nil))
+(def ^:private store-last-message-interceptor
+  {:name ::store-last-message-interceptor
+   :enter (fn [{:as ctx :keys [message]}]
+            (reset! last-message-store message)
+            ctx)})
+
 (def config
   (let [f (io/file "dev" "elin" "config.edn")]
     (merge {:server {:host "nvim"
-                     :port 12233}}
+                     :port 12233}
+            :interceptor {:manager {e.c.kind/handler [store-last-message-interceptor]}}}
            (if (.exists f)
              (edn/read-string (slurp f))
              {}))))
@@ -49,3 +58,7 @@
   []
   (stop)
   (start))
+
+(defn last-message
+  []
+  @last-message-store)
