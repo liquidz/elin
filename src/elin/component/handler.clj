279654fc881e127673a297b4d55275437f8aa541
liquidz <liquidz.uo@gmail.com>
2024-01-22 22:24:34 +0900

    fix: Fix handler component to pass writer

--- src/elin/component/handler.clj
+++ src/elin/component/handler.clj
@@ -6,29 +6,36 @@
    [elin.log :as e.log]
    [elin.protocol.interceptor :as e.p.interceptor]
    [elin.protocol.rpc :as e.p.rpc]
+   [elin.schema.handler :as e.s.handler]
+   [malli.core :as m]
    [msgpack.clojure-extensions]))
 
-(defn- generate-handler
-  [{:as components :component/keys [interceptor]}]
-  (fn [msg]
-    (-> {:message msg}
-        (as-> ctx
-          (e.p.interceptor/execute
-           interceptor e.c.kind/handler ctx
-           (fn [{:as context :keys [message]}]
-             (let [msg' (merge message
-                               (e.p.rpc/parse-message message))
-                   params (assoc components
-                                 :message msg')
-                   resp (e.h.core/handler* params)
-                   resp' (if-let [callback (:callback msg')]
-                           (try
-                             (e.p.rpc/call-function msg "elin#callback#call" [callback resp])
-                             (catch Exception ex
-                               (e.log/error msg "Failed to callback" (ex-message ex))))
-                           resp)]
-               (assoc context :response resp')))))
-        (:response))))
+(m/=> handler [:=> [:cat
+                    [:map-of keyword? any?]
+                    e.s.handler/?ArgMap]
+               any?])
+(defn- handler
+  [{:as components :component/keys [interceptor]}
+   arg-map]
+  (-> arg-map
+      (as-> ctx
+        (e.p.interceptor/execute
+         interceptor e.c.kind/handler ctx
+         (fn [{:as context :keys [message writer]}]
+           (let [msg' (merge message
+                             (e.p.rpc/parse-message message))
+                 params (assoc components
+                               :message msg'
+                               :writer writer)
+                 resp (e.h.core/handler* params)
+                 resp' (if-let [callback (:callback msg')]
+                         (try
+                           (e.p.rpc/call-function writer "elin#callback#call" [callback resp])
+                           (catch Exception ex
+                             (e.log/error writer "Failed to callback" (ex-message ex))))
+                         resp)]
+             (assoc context :response resp')))))
+      (:response)))
 
 (defrecord Handler
   [nrepl interceptor]
@@ -36,7 +43,7 @@
   (start [this]
     (let [components {:component/nrepl nrepl
                       :component/interceptor interceptor}
-          handler (generate-handler components)]
+          handler (partial handler components)]
       (assoc this
              :handler handler)))
 
