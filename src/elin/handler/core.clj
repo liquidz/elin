1615a1e9a80f5b5dedc1518f37c19af9a618df50
liquidz <liquidz.uo@gmail.com>
2024-01-22 22:30:28 +0900

    fix: Fix handlers to use writer

--- src/elin/handler/core.clj
+++ src/elin/handler/core.clj
@@ -7,8 +7,7 @@
    [elin.log :as e.log]
    [elin.nrepl.message :as e.n.message]
    [elin.protocol.interceptor :as e.p.interceptor]
-   [elin.protocol.nrepl :as e.p.nrepl]
-   [elin.protocol.rpc :as e.p.rpc]))
+   [elin.protocol.nrepl :as e.p.nrepl]))
 
 (defmulti handler* (comp :method :message))
 (defmethod handler* :default [_] nil)
@@ -19,14 +18,13 @@
   "FIXME")
 
 (defmethod handler* :connect
-  [{:component/keys [nrepl interceptor] :keys [message]}]
+  [{:as elin :component/keys [nrepl interceptor] :keys [message writer]}]
   (let [{:keys [params]} message
         [host port] (condp = (count params)
                       0 [nil nil]
                       1 [nil (first params)]
                       params)
-        cwd (e.f.host/get-current-working-directory message)
-        result (-> {:message message :cwd cwd :host host :port port}
+        result (-> {:elin elin :host host :port port}
                    (as-> ctx
                      (e.p.interceptor/execute
                       interceptor e.c.kind/connect ctx
@@ -37,12 +35,13 @@
                             (assoc ctx :client client))
                           ctx)))))]
     (if (contains? result :client)
-      (e.log/info message (format "Connected to %s:%s" (:host result) (:port result)))
-      (e.log/warning message "Host or port is not specified." (pr-str (select-keys result [:host :port]))))))
+      (e.log/info writer (format "Connected to %s:%s" (:host result) (:port result)))
+      (e.log/warning writer "Host or port is not specified." (pr-str (select-keys result [:host :port]))))))
 
 (defn- evaluation*
-  [{:component/keys [nrepl interceptor]} code & [options]]
-  (-> {:code code :options (or options {})}
+  [{:as elin :component/keys [nrepl interceptor]}
+   code & [options]]
+  (-> {:elin elin :code code :options (or options {})}
       (as-> ctx
         (e.p.interceptor/execute
          interceptor e.c.kind/evaluate ctx
@@ -53,19 +52,32 @@
       (get-in [:response :value])))
 
 (defmethod handler* :evaluate
-  [{:as msg :keys [message]}]
+  [{:as elin :keys [message]}]
   (->> message
        (:params)
        (first)
-       (evaluation* msg)))
-
-(defmethod handler* :plus
-  [{:as msg :keys [params]}]
-  (let [res (async/<!! (e.p.rpc/call-function msg "elin#plus_test" params))]
-    (e.log/info msg "FIXME plus result" (pr-str res))))
+       (evaluation* elin)))
 
 (defmethod handler* :evaluate-current-top-list
-  [{:as msg :keys [message]}]
-  (let [{:keys [lnum col]} (e.f.host/get-cursor-position message)
-        code (e.f.sexp/get-current-top-list message lnum col)]
-    (evaluation* msg code)))
+  [{:as elin :keys [writer]}]
+  (let [{:keys [lnum col]} (e.f.host/get-cursor-position writer)
+        code (e.f.sexp/get-top-list writer lnum col)
+        option {:line lnum
+                :column col}]
+    (evaluation* elin code option)))
+
+(defmethod handler* :evaluate-current-list
+  [{:as elin :keys [writer]}]
+  (let [{:keys [lnum col]} (e.f.host/get-cursor-position writer)
+        code (e.f.sexp/get-list writer lnum col)
+        res (evaluation* elin code)]
+    res))
+
+(defmethod handler* :evaluate-current-expr
+  [{:as elin :keys [writer]}]
+  (let [{:keys [lnum col]} (e.f.host/get-cursor-position writer)
+        code (e.f.sexp/get-expr writer lnum col)]
+    (evaluation* elin code)))
+
+(defmethod handler* :test
+  [_elin] "foo")
