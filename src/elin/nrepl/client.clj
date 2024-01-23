c911e6bf94a18769f9c76407afeede744c2297d3
liquidz <liquidz.uo@gmail.com>
2024-01-23 22:11:04 +0900

    fix: Rmove INreplOp implementations from Client

--- src/elin/nrepl/client.clj
+++ src/elin/nrepl/client.clj
@@ -2,7 +2,6 @@
   (:require
    [clojure.core.async :as async]
    [elin.nrepl.connection :as e.n.connection]
-   [elin.nrepl.constant :as e.n.constant]
    [elin.nrepl.message :as e.n.message]
    [elin.protocol.nrepl :as e.p.nrepl]
    [malli.core :as m]))
@@ -26,7 +25,7 @@
    session
    supported-ops
    initial-namespace
-   versions]
+   version]
 
   e.p.nrepl/IConnection
   (disconnect [_]
@@ -35,14 +34,14 @@
     (e.p.nrepl/disconnected? connection))
   (notify [this msg]
     (when-not (e.p.nrepl/supported-op? this (:op msg))
-      (throw (Exception. "FIXME not supported op")))
+      (throw (ex-info "Not supported operation" {:message msg})))
     (let [msg (if (contains? msg :session)
                 msg
                 (assoc msg :session session))]
       (e.p.nrepl/notify connection msg)))
   (request [this msg]
     (when-not (e.p.nrepl/supported-op? this (:op msg))
-      (throw (Exception. "FIXME not supported op")))
+      (throw (ex-info "Not supported operation" {:message msg})))
     (let [msg (if (contains? msg :session)
                 msg
                 (assoc msg :session session))]
@@ -50,26 +49,7 @@
 
   e.p.nrepl/IClient
   (supported-op? [_ op]
-    (contains? supported-ops (keyword op)))
-
-  e.p.nrepl/INreplOp
-  (close-op [this]
-    (e.p.nrepl/request this {:op "close" :session session}))
-
-  (eval-op [this code options]
-    (->> (merge (select-keys options e.n.constant/eval-option-keys)
-                {:op "eval" :session session :code code})
-         (e.p.nrepl/request this)))
-
-  (interrupt-op [this options]
-    (->> (merge (select-keys options #{:interrupt-id})
-                {:op "interrupt" :session session})
-         (e.p.nrepl/request this)))
-
-  (load-file-op [this file options]
-    (->> (merge (select-keys options e.n.constant/load-file-option-keys)
-                {:op "load-file" :session session :file file})
-         (e.p.nrepl/request this))))
+    (contains? supported-ops (keyword op))))
 
 (m/=> connect [:=> [:cat string? int?] ?Client])
 (defn connect
@@ -79,19 +59,9 @@
                     (async/<!! (e.p.nrepl/request conn {:op "clone"})))
         describe-resp (e.n.message/merge-messages
                        (async/<!! (e.p.nrepl/request conn {:op "describe"})))]
-    (println (pr-str describe-resp))
     (map->Client
      {:connection conn
       :session (:new-session clone-resp)
       :supported-ops (set (keys (:ops describe-resp)))
       :initial-namespace (get-in describe-resp [:aux :current-ns])
       :version (:versions describe-resp)})))
-
-(comment
-  (let [client (connect "localhost" 61081)]
-    (try
-      (println "connected")
-      (println "resp" (async/<!! (e.p.nrepl/request client {:op "eval" :code "(+ 1 2 3)"})))
-      (finally
-        (e.p.nrepl/disconnect client)))))
-
