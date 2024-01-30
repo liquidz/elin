e1e5671a9445b3ee566b95509851c806d8b73f3c
liquidz <liquidz.uo@gmail.com>
2024-01-30 17:13:31 +0900

    refactor: Move merge-messages and update-messages to util.nrepl

---
+++ src/elin/util/nrepl.clj
@@ -0,0 +1,33 @@
+(ns elin.util.nrepl
+  (:require
+   [elin.constant.nrepl :as e.c.nrepl]
+   [elin.schema.nrepl :as e.s.nrepl]
+   [malli.core :as m]))
+
+(def ^:private ?Messages
+  [:sequential e.s.nrepl/?Message])
+
+(m/=> merge-messages [:=> [:cat ?Messages] e.s.nrepl/?Message])
+(defn merge-messages
+  [messages]
+  (let [array-keys (map keyword e.c.nrepl/array-key-set)
+        array-res (reduce (fn [accm k]
+                            (if-let [arr (some->> (keep k messages)
+                                                  (seq)
+                                                  (apply concat)
+                                                  (distinct))]
+                              (assoc accm k arr)
+                              accm))
+                          {} array-keys)]
+    (->> (map #(apply dissoc % array-keys) messages)
+         (apply merge array-res))))
+
+(m/=> update-messages [:=> [:cat keyword? fn? ?Messages] ?Messages])
+(defn update-messages
+  [k f messages]
+  (map
+   (fn [res]
+     (cond-> res
+       (contains? res k)
+       (update k f)))
+   messages))
