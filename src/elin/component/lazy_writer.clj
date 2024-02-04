a68d1b14db2b018ca78df809a08e9d1d2918a06e
liquidz <liquidz.uo@gmail.com>
2024-02-04 22:31:32 +0900

    fix: Fix lazy-writer component to work request! and call-function correctly

--- src/elin/component/lazy_writer.clj
+++ src/elin/component/lazy_writer.clj
@@ -15,11 +15,15 @@
         (if-let [writer @writer-store]
           (let [[type & args] (async/<! ch)]
             (case type
-              ::request! (apply e.p.rpc/request! writer args)
+              ::request! (let [[ch & args] args
+                               res-ch (apply e.p.rpc/request! writer args)]
+                           (async/pipe res-ch ch))
               ::notify! (apply e.p.rpc/notify! writer args)
               ::response! (apply e.p.rpc/response! writer args)
               ::flush! (e.p.rpc/flush! writer)
-              ::call-function (apply e.p.rpc/call-function writer args)
+              ::call-function (let [[ch & args] args
+                                    res-ch (apply e.p.rpc/call-function writer args)]
+                                (async/pipe res-ch ch))
               ::notify-function (apply e.p.rpc/notify-function writer args)
               ::echo-text (apply e.p.rpc/echo-text writer args)
               ::echo-message (apply e.p.rpc/echo-message writer args)
@@ -27,7 +31,7 @@
             (when type
               (recur)))
           (do
-            (async/<! (async/timeout 1000))
+            (async/<! (async/timeout 100))
             (recur))))
 
       (assoc this :writer-channel ch)))
@@ -45,7 +49,9 @@
   (request! [_ content]
     (if-let [writer @writer-store]
       (e.p.rpc/request! writer content)
-      (async/put! writer-channel [::request! content])))
+      (let [ch (async/promise-chan)]
+        (async/put! writer-channel [::request! ch content])
+        ch)))
   (notify! [_ content]
     (if-let [writer @writer-store]
       (e.p.rpc/notify! writer content)
@@ -63,7 +69,9 @@
   (call-function [_ method params]
     (if-let [writer @writer-store]
       (e.p.rpc/call-function writer method params)
-      (async/put! writer-channel [::call-function method params])))
+      (let [ch (async/promise-chan)]
+        (async/put! writer-channel [::call-function ch method params])
+        ch)))
   (notify-function [_ method params]
     (if-let [writer @writer-store]
       (e.p.rpc/notify-function writer method params)
