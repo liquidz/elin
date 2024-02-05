6c220a9c3ac85059493df763ec79f93df2182a47
liquidz <liquidz.uo@gmail.com>
2024-02-05 22:51:40 +0900

    fix: Change component dependencies

--- src/elin/system.clj
+++ src/elin/system.clj
@@ -15,18 +15,29 @@
   ([config]
    (component/system-map
     :lazy-writer (e.c.lazy-writer/new-lazy-writer config)
-    :interceptor (component/using
-                  (e.c.interceptor/new-interceptor config)
-                  [:lazy-writer])
-    :nrepl (component/using
-            (e.c.nrepl/new-nrepl config)
-            [:interceptor :lazy-writer])
-    :handler (component/using
-              (e.c.handler/new-handler config)
-              [:nrepl :interceptor :lazy-writer])
+
     :plugin (component/using
              (e.c.plugin/new-plugin config)
-             [:interceptor :handler :lazy-writer])
+             [:lazy-writer])
+
+    :interceptor (component/using
+                  (e.c.interceptor/new-interceptor config)
+                  [:lazy-writer
+                   :plugin])
+
+    :nrepl (component/using
+            (e.c.nrepl/new-nrepl config)
+            [:interceptor
+             :lazy-writer])
+
+    :handler (component/using
+              (e.c.handler/new-handler config)
+              [:interceptor
+               :lazy-writer
+               :nrepl
+               :plugin])
+
     :server (component/using
              (e.c.server/new-server config)
-             [:handler :lazy-writer]))))
+             [:handler
+              :lazy-writer]))))
