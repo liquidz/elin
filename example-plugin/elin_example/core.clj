262fd7f5dc589529913058e9ee1ef55b1c457559
liquidz <liquidz.uo@gmail.com>
2024-02-06 21:48:05 +0900

    test: Add tests for plugin component

---
+++ example-plugin/elin_example/core.clj
@@ -0,0 +1,8 @@
+(ns elin-example.core)
+
+(defn hello
+  [_]
+  "Hello world")
+
+(def interceptor
+  {:name ::interceptor})
---
+++ test/elin/component/plugin_test.clj
@@ -0,0 +1,36 @@
+(ns elin.component.plugin-test
+  (:require
+   [clojure.java.io :as io]
+   [clojure.test :as t]
+   [com.stuartsierra.component :as component]
+   [elin.component.plugin :as sut]
+   [elin.system :as e.system]
+   [elin.test-helper :as h]))
+
+(t/use-fixtures :once h/malli-instrument-fixture)
+
+(t/deftest new-plugin-test
+  (let [edn-file (.getAbsolutePath (io/file "example-plugin" "plugin.edn"))
+        config {:plugin {:edn-files [edn-file]}}
+        {:as sys :keys [plugin]} (-> (e.system/new-system config)
+                                     (select-keys [:lazy-writer :plugin])
+                                     (component/start-system))]
+    (try
+      (t/is (= {:name (str ::sut/plugin)
+                :handlers '[elin-example.core/hello]
+                :interceptors '[elin-example.core/interceptor]}
+               (:loaded-plugin plugin)))
+      (finally
+        (component/stop-system sys)))))
+
+(t/deftest new-plugin-no-plugin-test
+  (let [{:as sys :keys [plugin]} (-> (e.system/new-system)
+                                     (select-keys [:lazy-writer :plugin])
+                                     (component/start-system))]
+    (try
+      (t/is (= {:name (str ::sut/plugin)
+                :handlers []
+                :interceptors []}
+               (:loaded-plugin plugin)))
+      (finally
+        (component/stop-system sys)))))
