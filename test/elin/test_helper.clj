bf9b8b6cf457d7a8b220b45d937c31e1fd9d18c6
liquidz <liquidz.uo@gmail.com>
2024-02-08 23:49:09 +0900

    test: Add test-nrepl-connection, test-nrepl-client, and test-nrepl to test helper

--- test/elin/test_helper.clj
+++ test/elin/test_helper.clj
@@ -2,6 +2,10 @@
   (:require
    [babashka.nrepl.server :as b.n.server]
    [clojure.core.async :as async]
+   [elin.component.interceptor :as e.c.interceptor]
+   [elin.component.nrepl :as e.c.nrepl]
+   [elin.component.nrepl.client :as e.c.n.client]
+   [elin.protocol.nrepl :as e.p.nrepl]
    [elin.protocol.rpc :as e.p.rpc]
    [elin.schema.server :as e.s.server]
    [elin.util.id :as e.u.id]
@@ -11,6 +15,7 @@
 
 (def ^:dynamic *nrepl-server-port* nil)
 
+;; FIXTURES {{{
 (defn malli-instrument-fixture
   [f]
   (m.inst/instrument!
@@ -26,15 +31,15 @@
         (f))
       (finally
         (b.n.server/stop-server! server)))))
+;; }}}
+
+;; RECORDS {{{
 
 (def ?TestMessageOption
   [:map
    [:handler [:=> [:cat [:sequential any?]] any?]]])
 
-(defprotocol ITestWriter
-  (get-outputs [this]))
-
-(defrecord TestMessage
+(defrecord TestMessage ; {{{
   [host message]
   e.p.rpc/IMessage
   (request? [_]
@@ -60,9 +65,12 @@
           {:method (keyword method)
            :params params
            :callback callback})
-      {})))
+      {}))) ; }}}
 
-(defrecord TestWriter
+(defprotocol ITestWriter
+  (get-outputs [this]))
+
+(defrecord TestWriter ; {{{
   [output-stream outputs option]
   e.p.rpc/IWriter
   (request! [_ content]
@@ -97,8 +105,49 @@
     (swap! outputs conj text))
 
   ITestWriter
-  (get-outputs [_] @outputs))
+  (get-outputs [_] @outputs)) ; }}}
 
+(defn- nrepl-connectin-default-handler
+  [msg]
+  (condp = (:op msg)
+    "clone"
+    [{:new-session "dummy-session"}]
+
+    "describe"
+    [{:ops {:clone 1 :describe 1 :eval 1 :lookup 1}
+      :versions {:elin "test"}}]
+
+    "eval"
+    (when (= (str '(ns-name *ns*)) (:code msg))
+      [{:value "user"}])
+
+    nil))
+
+(defrecord TestNreplConnection ; {{{
+  [host port socket read-stream write-stream output-channel response-manager
+   connected-atom option]
+  e.p.nrepl/IConnection
+  (disconnect [_]
+    (reset! connected-atom false))
+
+  (disconnected? [_]
+    (not @connected-atom))
+
+  (notify [this msg]
+    (when-not (e.p.nrepl/disconnected? this)
+      (let [option-handler (or (:handler option) (constantly nil))
+            res (option-handler msg)]
+        (if res
+          res
+          (nrepl-connectin-default-handler msg)))))
+
+  (request [this msg]
+    (async/go
+      (e.p.nrepl/notify this msg)))) ; }}}
+
+;; }}}
+
+;; FUNCTIONS {{{
 (m/=> test-message [:function
                     [:=> :cat e.s.server/?Message]
                     [:=> [:cat [:sequential any?]] e.s.server/?Message]])
@@ -116,7 +165,39 @@
                     :outputs (atom [])
                     :option option}))
 
+(defn test-nrepl-connection
+  [option]
+  (map->TestNreplConnection
+   {:host "localhost"
+    :port 1234
+    :socket (java.net.Socket.)
+    :read-stream (java.io.PushbackInputStream.
+                  (java.io.ByteArrayInputStream. (.getBytes "")))
+    :write-stream (java.io.ByteArrayOutputStream.)
+    :output-channel (async/chan)
+    :response-manager (atom {})
+    :connected-atom (atom true)
+    :option option}))
+
+(defn test-nrepl-client
+  [option]
+  (-> (test-nrepl-connection option)
+      (e.c.n.client/new-client)))
+
+(defn test-nrepl
+  [option]
+  (let [writer (test-writer (or (:lazy-writer option) {}))
+        interceptor (e.c.interceptor/new-interceptor (or (:interceptor option) {}))
+        client (test-nrepl-client (or (:client option) {}))
+        nrepl (e.c.nrepl/new-nrepl
+               {:nrepl {:interceptor interceptor
+                        :lazy-writer writer}})]
+    (e.p.nrepl/add-client! nrepl client)
+    (e.p.nrepl/switch-client! nrepl client)
+    nrepl))
+
 (defn call-function? [msg fn-name]
   (and
    (= "test_call_function" (nth msg 2))
    (= fn-name (first (nth msg 3)))))
+;; }}}
