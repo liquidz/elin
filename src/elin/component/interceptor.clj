b6d93cc9b15688dd8aaf30e9b7c6586635a91bb7
liquidz <liquidz.uo@gmail.com>
2024-02-15 21:30:21 +0900

    feat: Add configure implementation

--- src/elin/component/interceptor.clj
+++ src/elin/component/interceptor.clj
@@ -7,11 +7,14 @@
    [elin.interceptor.nrepl]
    [elin.interceptor.output]
    [elin.log :as e.log]
+   [elin.protocol.config :as e.p.config]
    [elin.protocol.interceptor :as e.p.interceptor]
    [elin.schema.interceptor :as e.s.interceptor]
    [exoscale.interceptor :as interceptor]
    [malli.core :as m]
    [msgpack.clojure-extensions]))
+
+(def ^:private config-key :interceptor)
 
 (def ^:private default-interceptors
   '[elin.interceptor.connect/port-auto-detecting-interceptor
@@ -75,8 +78,30 @@
       (try
         (interceptor/execute context' (concat interceptors [terminator']))
         (catch Exception ex
-          (e.log/error "Interceptor error" ex))))))
+          (e.log/error lazy-writer "Failed to intercept:" (ex-message ex))))))
+
+  e.p.config/IConfigure
+  (configure [this config]
+    (let [{:keys [includes excludes]} (get config config-key)
+          exclude-set (->> (or excludes [])
+                           (map keyword)
+                           (set))
+          _ (e.log/info "FIXME excludes" (pr-str exclude-set))
+          grouped (->> (or includes [])
+                       (keep #(resolve-interceptor lazy-writer %))
+                       (group-by valid-interceptor?))
+          include-map (group-by :kind (get grouped true))
+          configured (reduce-kv
+                      (fn [accm kind interceptors]
+                        (assoc accm kind (concat (get accm kind []) interceptors)))
+                      interceptor-map
+                      include-map)
+          configured (update-vals configured
+                                  (fn [interceptors]
+                                    (remove #(contains? exclude-set (:name %)) interceptors)))
+          _ (e.log/info "FIXME configured" (pr-str (get configured e.c.interceptor/nrepl)))]
+      (assoc this :interceptor-map configured))))
 
 (defn new-interceptor
   [config]
-  (map->Interceptor (or (:interceptor config) {})))
+  (map->Interceptor (or (get config config-key) {})))
