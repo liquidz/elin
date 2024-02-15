95491f0c617c6e7a505f6b2ffedac38012d0e75f
liquidz <liquidz.uo@gmail.com>
2024-02-15 23:03:02 +0900

    test: Fix elin.test-helper.message

--- test/elin/test_helper/message.clj
+++ test/elin/test_helper/message.clj
@@ -2,6 +2,7 @@
   (:require
    [elin.protocol.rpc :as e.p.rpc]
    [elin.schema.server :as e.s.server]
+   [elin.util.server :as e.u.server]
    [malli.core :as m]))
 
 (def ?TestMessageOption
@@ -20,20 +21,23 @@
   (parse-message [_]
     (condp = (first message)
       ;; request
-      0 (let [[_ id method [params]] message]
+      0 (let [[_ id method [params options]] message]
           {:id id
            :method (keyword method)
-           :params params})
+           :params params
+           :options (-> (e.u.server/unformat options)
+                        ;; Do not allow 'callback' option for request
+                        (dissoc :callback))})
       ;; response
       1 (let [[_ id error result] message]
           {:id id
            :error error
            :result result})
       ;; notify
-      2 (let [[_ method [params callback]] message]
+      2 (let [[_ method [params options]] message]
           {:method (keyword method)
            :params params
-           :callback callback})
+           :options (e.u.server/unformat options)})
       {}))) ; }}}
 
 (m/=> test-message [:function
