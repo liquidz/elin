932f81a728814f8ff64c302e38c53c1da44c6a43
liquidz <liquidz.uo@gmail.com>
2024-02-18 00:12:03 +0900

    refactor: Fix elin.handler.evaluate to use elin.function.nrepl.vim

--- src/elin/handler/evaluate.clj
+++ src/elin/handler/evaluate.clj
@@ -2,69 +2,39 @@
   (:require
    [elin.error :as e]
    [elin.function.nrepl :as e.f.nrepl]
+   [elin.function.nrepl.vim :as e.f.n.vim]
    [elin.function.vim :as e.f.vim]
-   [elin.function.vim.sexp :as e.f.v.sexp]
    [elin.schema.handler :as e.s.handler]
    [malli.core :as m]))
-
-(m/=> evaluation* [:=> [:cat e.s.handler/?Elin string? map?] any?])
-(defn- evaluation*
-  [{:component/keys [nrepl]}
-   code options]
-  (e/let [options (reduce-kv (fn [accm k v]
-                               (if v
-                                 (assoc accm k v)
-                                 accm))
-                             {:nrepl.middleware.print/stream? 1}
-                             options)
-          res (e.f.nrepl/eval!! nrepl code options)]
-    (:value res)))
 
 ;; TODO status: ["namespace-not-found" "done" "error"]
 
 (m/=> evaluate [:=> [:cat e.s.handler/?Elin] any?])
 (defn evaluate
-  [{:as elin :keys [message]}]
-  (let [code (->> message
-                  (:params)
-                  (first))]
-    (evaluation* elin code {})))
+  [{:component/keys [nrepl] :keys [message]}]
+  (e/let [code (->> message
+                    (:params)
+                    (first))
+          res (e.f.nrepl/eval!! nrepl code {})]
+    (:value res)))
 
 (m/=> evaluate-current-top-list [:=> [:cat e.s.handler/?Elin] any?])
 (defn evaluate-current-top-list
-  [{:as elin :component/keys [host]}]
-  (e/let [{:keys [lnum col]} (e.f.vim/get-cursor-position!! host)
-          ns-str (e.f.v.sexp/get-namespace!! host)
-          path (e.f.vim/get-full-path!! host)
-          {:keys [code lnum col]} (e.f.v.sexp/get-top-list!! host lnum col)]
-    (evaluation* elin code {:line lnum
-                            :column col
-                            :ns ns-str
-                            :file path})))
+  [{:component/keys [host nrepl]}]
+  (e/-> (e.f.n.vim/evaluate-current-top-list!! {:host host :nrepl nrepl})
+        (:response)))
 
 (m/=> evaluate-current-list [:=> [:cat e.s.handler/?Elin] any?])
 (defn evaluate-current-list
-  [{:as elin :component/keys [host]}]
-  (e/let [{:keys [lnum col]} (e.f.vim/get-cursor-position!! host)
-          ns-str (e.f.v.sexp/get-namespace!! host)
-          path (e.f.vim/get-full-path!! host)
-          {:keys [code lnum col]} (e.f.v.sexp/get-list!! host lnum col)]
-    (evaluation* elin code {:line lnum
-                            :column col
-                            :ns ns-str
-                            :file path})))
+  [{:component/keys [host nrepl]}]
+  (e/-> (e.f.n.vim/evaluate-current-list!! {:host host :nrepl nrepl})
+        (:response)))
 
 (m/=> evaluate-current-expr [:=> [:cat e.s.handler/?Elin] any?])
 (defn evaluate-current-expr
-  [{:as elin :component/keys [host]}]
-  (e/let [{:keys [lnum col]} (e.f.vim/get-cursor-position!! host)
-          ns-str (e.f.v.sexp/get-namespace!! host)
-          path (e.f.vim/get-full-path!! host)
-          {:keys [code lnum col]} (e.f.v.sexp/get-expr!! host lnum col)]
-    (evaluation* elin code {:line lnum
-                            :column col
-                            :ns ns-str
-                            :file path})))
+  [{:component/keys [host nrepl]}]
+  (e/-> (e.f.n.vim/evaluate-current-expr!! {:host host :nrepl nrepl})
+        (:response)))
 
 (m/=> load-current-file [:=> [:cat e.s.handler/?Elin] any?])
 (defn load-current-file
