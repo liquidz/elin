8a226462fab7bce5c92fecf27273ac185be4722c
liquidz <liquidz.uo@gmail.com>
2024-02-18 18:09:11 +0900

    feat: Add elin.util.map/map->str

--- src/elin/util/map.clj
+++ src/elin/util/map.clj
@@ -1,5 +1,6 @@
 (ns elin.util.map
   (:require
+   [clojure.string :as str]
    [malli.core :as m]))
 
 (m/=> select-keys-by-namespace [:=> [:cat map? keyword?] map?])
@@ -10,3 +11,15 @@
                  (assoc accm k v)
                  accm))
              {} m))
+
+(m/=> map->str [:=> [:cat map? [:sequential keyword?]] string?])
+(defn map->str [m keyseq]
+  (let [max-key-length (apply max (map #(count (name %)) keyseq))]
+    (->> (select-keys m keyseq)
+         (mapcat (fn [[k v]]
+                   (let [lines (str/split-lines v)]
+                     (cons (format (str "%" max-key-length "s: %s") (name k) (first lines))
+                           (map #(str (apply str (repeat (+ 2 max-key-length) " "))
+                                      %)
+                                (rest lines))))))
+         (str/join "\n"))))
