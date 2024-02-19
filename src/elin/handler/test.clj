53c41d86dd08dc8391bffb5845dbddc0bf9a66a9
liquidz <liquidz.uo@gmail.com>
2024-02-19 22:21:19 +0900

    fix: Update elin.handler.test/run-test-under-cursor to support plain nREPL

--- src/elin/handler/test.clj
+++ src/elin/handler/test.clj
@@ -5,6 +5,7 @@
    [elin.constant.nrepl :as e.c.nrepl]
    [elin.error :as e]
    [elin.function.nrepl.cider :as e.f.n.cider]
+   [elin.function.nrepl.test :as e.f.n.test]
    [elin.function.nrepl.vim :as e.f.n.vim]
    [elin.protocol.interceptor :as e.p.interceptor]
    [elin.protocol.nrepl :as e.p.nrepl]
@@ -23,20 +24,29 @@
 
 (defn run-test-under-cursor
   [{:as elin :component/keys [host nrepl interceptor]}]
-  (if (e.p.nrepl/supported-op? nrepl e.c.nrepl/test-var-query-op)
-    (e/let [{:keys [code response options]} (e.f.n.vim/evaluate-current-top-list!! {:host host :nrepl nrepl})
-            {ns-str :ns} options
-            var-name (or (some->> (extract-multi-method-name code)
-                                  (str ns-str "/"))
-                         (str/replace (:value response) #"^#'" ""))
-            context (-> (e.u.map/select-keys-by-namespace elin :component)
-                        (assoc :ns ns-str
-                               :vars [var-name]))]
-      (e.p.interceptor/execute
-       interceptor e.c.interceptor/test context
-       (fn [ctx]
+  (e/let [{:keys [code response options]} (e.f.n.vim/evaluate-current-top-list!! {:host host :nrepl nrepl})
+          {ns-str :ns} options
+          var-name (or (some->> (extract-multi-method-name code)
+                                (str ns-str "/"))
+                       (str/replace (:value response) #"^#'" ""))
+          context (-> (e.u.map/select-keys-by-namespace elin :component)
+                      (assoc :ns ns-str
+                             :line (:line options)
+                             :column (:column options)
+                             :file (:file options)
+                             :vars [var-name]))]
+    (e.p.interceptor/execute
+     interceptor e.c.interceptor/test context
+     (fn [{:as ctx :component/keys [nrepl]}]
+       (if (e.p.nrepl/supported-op? nrepl e.c.nrepl/test-var-query-op)
+         ;; cider-nrepl
          (let [query {:ns-query {:exactly [(:ns ctx)]}
                       :exactly (:vars ctx)}]
-           (assoc ctx :response (e.f.n.cider/test-var-query!! nrepl query))))))
+           (assoc ctx :response (e.f.n.cider/test-var-query!! nrepl query)))
 
-    "TODO plain"))
+         ;; plain
+         (let [query {:ns (:ns ctx)
+                      :vars (:vars ctx)
+                      :current-file (:file ctx)
+                      :base-line (:line ctx)}]
+           (assoc ctx :response (e.f.n.test/test-var-query!! nrepl query))))))))
