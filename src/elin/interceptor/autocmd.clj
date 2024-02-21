dbe9d3965436b2bd6153699adfbf60472a29ae22
liquidz <liquidz.uo@gmail.com>
2024-02-22 00:30:13 +0900

    feat: Add setting-http-server-port-interceptor

--- src/elin/interceptor/autocmd.clj
+++ src/elin/interceptor/autocmd.clj
@@ -1,11 +1,13 @@
 (ns elin.interceptor.autocmd
   (:require
    [elin.constant.interceptor :as e.c.interceptor]
+   [elin.constant.server :as e.c.server]
    [elin.error :as e]
    [elin.function.nrepl :as e.f.nrepl]
    [elin.function.vim :as e.f.vim]
    [elin.function.vim.sexp :as e.f.v.sexp]
-   [elin.protocol.nrepl :as e.p.nrepl]))
+   [elin.protocol.nrepl :as e.p.nrepl]
+   [elin.protocol.storage :as e.p.storage]))
 
 (def ^:priavte ns-created-var-name
   "b:elin_ns_created")
@@ -27,4 +29,19 @@
                      (str)
                      (e.f.nrepl/eval!! nrepl))
                 (e.f.vim/set-variable!! host ns-created-var-name true)))
+            ctx)})
+
+(def setting-http-server-port-interceptor
+  {:name ::setting-http-server-port-interceptor
+   :kind e.c.interceptor/autocmd
+   :enter (fn [{:as ctx :component/keys [host session-storage] :keys [autocmd-type]}]
+            (when (contains? #{"BufRead" "BufEnter"} autocmd-type)
+              (when-let [port (e.p.storage/get session-storage e.c.server/http-server-port-key)]
+                (e.f.vim/set-variable!! host
+                                        (str "g:" e.c.server/http-server-port-variable)
+                                        port)
+                ;; NOTE buffer scoped variable is used by coc.nvim
+                (e.f.vim/set-variable!! host
+                                        (str "b:" e.c.server/http-server-port-variable)
+                                        port)))
             ctx)})
