ffc2f1e58a832778a3bb833f689b9366449aa212
liquidz <liquidz.uo@gmail.com>
2024-02-24 03:10:40 +0900

    feat: Add skeleton-interceptor

--- src/elin/interceptor/autocmd.clj
+++ src/elin/interceptor/autocmd.clj
@@ -1,8 +1,10 @@
 (ns elin.interceptor.autocmd
   (:require
+   [clojure.string :as str]
    [elin.constant.interceptor :as e.c.interceptor]
    [elin.error :as e]
    [elin.function.nrepl :as e.f.nrepl]
+   [elin.function.nrepl.namespace :as e.f.n.namespace]
    [elin.function.vim :as e.f.vim]
    [elin.function.vim.sexp :as e.f.v.sexp]
    [elin.protocol.nrepl :as e.p.nrepl]
@@ -28,4 +30,40 @@
                          (str)
                          (e.f.nrepl/eval!! nrepl))
                     (e.f.vim/set-variable!! host ns-created-var-name true))))
+              (ix/discard))})
+
+(defmulti generate-skeleton
+  (fn [{:keys [lang test?]}]
+    [lang test?]))
+
+(defmethod generate-skeleton :default [_] nil)
+
+(defmethod generate-skeleton ["clojure" false]
+  [{:keys [ns-str]}]
+  (format "(ns %s)" ns-str))
+
+(defmethod generate-skeleton ["clojure" true]
+  [{:keys [ns-str]}]
+  (let [src-ns (str/replace ns-str #"-test$" "")]
+    (format "(ns %s\n  (:require\n   [clojure.test :as t]\n   [%s :as sut]))"
+            ns-str
+            src-ns)))
+
+(def skeleton-interceptor
+  {:name ::skelton-interceptor
+   :kind e.c.interceptor/autocmd
+   :enter (-> (fn [{:component/keys [host]}]
+                (e/let [path (e.f.vim/get-full-path!! host)
+                        ns-str (or (e.f.n.namespace/guess-namespace-from-path path)
+                                   ;; TODO fallback to another process
+                                   (e/fault))
+                        param {:path path
+                               :ns-str ns-str
+                               ;; TODO cljs, cljc support
+                               :lang "clojure"
+                               :test? (str/ends-with? ns-str "-test")}
+                        ns-form-lines (->> (generate-skeleton param)
+                                           (str/split-lines))]
+                  (e.f.vim/notify host "elin#internal#buffer#set" ["%" ns-form-lines])))
+              (ix/when #(= "BufNewFile" (:autocmd-type %)))
               (ix/discard))})
