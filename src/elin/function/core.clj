a97a22d16f8475483880bbbf9b5ee20cf2485f0a
liquidz <liquidz.uo@gmail.com>
2024-02-25 16:37:19 +0900

    feat: Add elin.function.core/lookup!!

---
+++ src/elin/function/core.clj
@@ -0,0 +1,25 @@
+(ns elin.function.core
+  (:require
+   [elin.error :as e]
+   [elin.function.clj-kondo :as e.f.clj-kondo]
+   [elin.function.nrepl.cider :as e.f.n.cider]
+   [elin.schema.handler :as e.s.handler]
+   [elin.schema.nrepl.op :as e.s.n.op]
+   [malli.core :as m]
+   [malli.util :as m.util]))
+
+(def ?NreplAndCljKondo
+  (m.util/select-keys e.s.handler/?Components [:component/nrepl :component/clj-kondo]))
+
+(m/=> lookup!! [:=> [:cat ?NreplAndCljKondo string? string?] e.s.n.op/?Lookup])
+(defn lookup!!
+  [{:component/keys [nrepl clj-kondo]} ns-str sym-str]
+  (let [res (e.f.n.cider/info!! nrepl ns-str sym-str)]
+    (if (e/error? res)
+      (try
+        (e.f.clj-kondo/lookup clj-kondo ns-str sym-str)
+        (catch Exception e
+          (e/fault {:message (pr-str e)})))
+      (do
+        (println "FIXME kotti???")
+        res))))
