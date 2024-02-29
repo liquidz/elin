6f01942007df9f9e390838bf94fad3022b778d1c
liquidz <liquidz.uo@gmail.com>
2024-02-29 21:58:13 +0900

    fix: Fix dev code to start server in nrepl session

--- dev/elin/task/nrepl.clj
+++ dev/elin/task/nrepl.clj
@@ -2,23 +2,27 @@
   (:require
    [babashka.fs :as fs]
    [babashka.nrepl.server :as srv]
+   [bencode.core :as b]
    [elin.dev :as e.dev]))
 
 (defn- start-dev-server!
-  [options]
-  (let [host (or (:host options) "nvim")
-        port (or (:port options) 45678)]
-    (e.dev/initialize {:host host :port port})
-    (e.dev/start)))
+  [{:keys [port options]}]
+  (with-open [s (java.net.Socket. "localhost" port)
+              output-stream (.getOutputStream s)]
+    (let [host (or (:host options) "nvim")
+          port (or (:port options) 45678)]
+      (b/write-bencode output-stream
+                       {"op" "eval"
+                        "code" (str `(do (elin.dev/initialize {:host ~host :port ~port})
+                                         (elin.dev/start)))}))))
 
 (defn -main
   [& [options]]
-  (start-dev-server! options)
-
   (let [{:keys [socket]} (srv/start-server! {:host "localhost" :port 0})
         port (.getLocalPort socket)]
     (spit ".nrepl-port" (str port))
     (-> (Runtime/getRuntime)
         (.addShutdownHook
          (Thread. (fn [] (fs/delete ".nrepl-port")))))
+    (start-dev-server! {:port port :options options})
     (deref (promise))))
