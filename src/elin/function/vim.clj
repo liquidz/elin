6ef57ab5e5d9bc72ca556513c90e99114ba27b22
liquidz <liquidz.uo@gmail.com>
2024-03-03 23:37:30 +0900

    feat: Add elin.function.vim/input!!

--- src/elin/function/vim.clj
+++ src/elin/function/vim.clj
@@ -105,3 +105,8 @@
   [host var-name value]
   (async/<!! (set-variable! host var-name value))
   nil)
+
+(m/=> input!! [:=> [:cat e.s.server/?Host string? string?] string?])
+(defn input!!
+  [host prompt default]
+  (call!! host "input" [prompt default]))
