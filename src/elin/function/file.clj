b94401682f72322e83d02ad5ff19b4961dc9c572
liquidz <liquidz.uo@gmail.com>
2024-04-23 15:50:11 +0900

    fix: Fix elin.function.file/open-as not to throw error on canceling input

--- src/elin/function/file.clj
+++ src/elin/function/file.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.core.async :as async]
    [clojure.java.io :as io]
+   [elin.error :as e]
    [elin.protocol.host :as e.p.host]))
 
 (defn open-as
@@ -9,6 +10,7 @@
   (if (.exists (io/file path))
     (async/<!! (e.p.host/jump! host path 1 1))
     (let [path' (async/<!! (e.p.host/input! host "Open this file?: " path))]
-      (when (seq path')
+      (when (and (not (e/fault? path'))
+                 (seq path'))
         (.mkdirs (.getParentFile (io/file path')))
         (async/<!! (e.p.host/jump! host path' 1 1))))))
