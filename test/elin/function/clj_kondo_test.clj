8b333eb403c3e9f984af41df46bb812e985f48ab
liquidz <liquidz.uo@gmail.com>
2024-07-15 07:57:35 +0900

    test: Add test for clj-kondo lookup

--- test/elin/function/clj_kondo_test.clj
+++ test/elin/function/clj_kondo_test.clj
@@ -18,3 +18,37 @@
     (t/is (= '[malli.core]
              (sut/requiring-namespaces c "elin.util.id")))
     (t/is (empty? (sut/requiring-namespaces c "non-existing-ns")))))
+
+(t/deftest lookup-test
+  (let [c (h/test-clj-kondo)]
+    (t/testing "not qualified symbol"
+      (t/is (= {:ns "elin.function.clj-kondo"
+                :name "lookup"
+                :file "src/elin/function/clj_kondo.clj"
+                :arglists-str ""}
+               (-> (sut/lookup c "elin.function.clj-kondo" "lookup")
+                   (dissoc :line :column)))))
+
+    (t/testing "qualified symbol with alias"
+      (t/is (= {:ns "elin.error"
+                :name "not-found"
+                :file "src/elin/error.clj"
+                :arglists-str ""}
+               (-> (sut/lookup c "elin.function.clj-kondo" "e/not-found")
+                   (dissoc :line :column)))))
+
+    (t/testing "qualified symbol without alias"
+      (t/is (= {:ns "elin.error"
+                :name "not-found"
+                :file "src/elin/error.clj"
+                :arglists-str ""}
+               (-> (sut/lookup c "elin.function.clj-kondo" "elin.error/not-found")
+                   (dissoc :line :column)))))
+
+    (t/testing "full namespace"
+      (t/is (= {:name "elin.error"
+                :file "src/elin/error.clj"
+                :ns ""
+                :arglists-str ""}
+               (-> (sut/lookup c "elin.function.clj-kondo" "elin.error")
+                   (dissoc :line :column)))))))
