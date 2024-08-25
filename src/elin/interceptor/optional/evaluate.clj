fdf9000132a701971cba075629f57755f80cc61b
liquidz <liquidz.uo@gmail.com>
2024-08-25 21:43:31 +0900

    feat: Add eval-with-context-interceptor

--- src/elin/interceptor/optional/evaluate.clj
+++ src/elin/interceptor/optional/evaluate.clj
@@ -1,6 +1,9 @@
 (ns elin.interceptor.optional.evaluate
   (:require
+   [clojure.core.async :as async]
    [elin.constant.interceptor :as e.c.interceptor]
+   [elin.protocol.host :as e.p.host]
+   [elin.protocol.storage :as e.p.storage]
    [elin.util.interceptor :as e.u.interceptor]))
 
 (def wrap-eval-code-interceptor
@@ -11,3 +14,17 @@
    :enter (fn [{:as ctx :keys [code]}]
             (let [{:keys [params]} (e.u.interceptor/self ctx)]
               (assoc ctx :code (format "(%s %s)" (first params) code))))})
+
+(def eval-with-context-interceptor
+  {:name ::eval-with-context-interceptor
+   :kind e.c.interceptor/evaluate
+   :optional true
+   :enter (fn [{:as ctx :elin/keys [kind] :component/keys [host session-storage] :keys [code]}]
+            (let [last-context (or (e.p.storage/get session-storage kind)
+                                   "")
+                  context (async/<!! (e.p.host/input! host "Evaluation context (let-style): " last-context))]
+              (if (seq context)
+                (do (e.p.storage/set session-storage kind context)
+                    (assoc ctx :code (format "(clojure.core/let [%s] %s)"
+                                             context code)))
+                ctx)))})
