8a697ecfab7dff93e3018869cac83f9944dc2750
liquidz <liquidz.uo@gmail.com>
2024-09-01 10:02:55 +0900

    fix: Fix port-auto-detecting-interceptor

--- src/elin/interceptor/connect.clj
+++ src/elin/interceptor/connect.clj
@@ -8,20 +8,30 @@
    [elin.util.map :as e.u.map]
    [exoscale.interceptor :as ix]))
 
+(def ^:private default-hostname "localhost")
+
 (def port-auto-detecting-interceptor
   {:name ::port-auto-detecting-interceptor
    :kind e.c.interceptor/connect
    :enter (fn [{:as ctx :component/keys [host] :keys [hostname port]}]
-            (if (and hostname port)
+            (cond
+              (and hostname port)
               ctx
-              (let [;; TODO error handling
-                    cwd (async/<!! (e.p.host/get-current-working-directory! host))
-                    nrepl-port-file (e.u.file/find-file-in-parent-directories cwd ".nrepl-port")
-                    hostname' (or hostname "localhost")
-                    port' (some-> nrepl-port-file
-                                  (slurp)
-                                  (Long/parseLong))]
-                (assoc ctx :hostname hostname' :port port'))))})
+
+              (and (not hostname) port)
+              (assoc ctx :hostname default-hostname)
+
+              :else
+              (if (and hostname port)
+                ctx
+                (let [;; TODO error handling
+                      cwd (async/<!! (e.p.host/get-current-working-directory! host))
+                      nrepl-port-file (e.u.file/find-file-in-parent-directories cwd ".nrepl-port")
+                      hostname' (or hostname default-hostname)
+                      port' (some-> nrepl-port-file
+                                    (slurp)
+                                    (Long/parseLong))]
+                  (assoc ctx :hostname hostname' :port port')))))})
 
 (def output-channel-interceptor
   {:name ::output-channel-interceptor
