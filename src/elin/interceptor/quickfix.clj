5894c091c97327e768defcf5e919f945a0aec388
liquidz <liquidz.uo@gmail.com>
2024-09-23 01:35:14 +0900

    feat: Add location-function-hook-interceptor

--- src/elin/interceptor/quickfix.clj
+++ src/elin/interceptor/quickfix.clj
@@ -5,7 +5,12 @@
    [elin.protocol.host :as e.p.host]
    [elin.protocol.rpc :as e.p.rpc]
    [elin.util.file :as e.u.file]
+   [elin.util.interceptor :as e.u.interceptor]
    [exoscale.interceptor :as ix]))
+
+(defn- location-list?
+  [context]
+  (= :location (:type context)))
 
 (def auto-toggling-vim-quickfix-window-interceptor
   {:kind e.c.interceptor/quickfix
@@ -36,12 +41,26 @@
 (def use-selector-for-location-interceptor
   {:kind e.c.interceptor/quickfix
    :leave (-> (fn [{:as ctx :component/keys [host]}]
-                (when (= :location (:type ctx))
-                  (let [candidates (->> (:list ctx)
-                                        (keep (fn [{:keys [filename lnum col]}]
-                                                (some-> filename
-                                                        (e.u.file/encode-path lnum col)))))]
-                    (when (seq candidates)
-                      (e.p.host/select-from-candidates
-                       host candidates 'elin.handler.navigate/jump)))))
+                (let [candidates (->> (:list ctx)
+                                      (keep (fn [{:keys [filename lnum col]}]
+                                              (some-> filename
+                                                      (e.u.file/encode-path lnum col)))))]
+                  (when (seq candidates)
+                    (e.p.host/select-from-candidates
+                     host candidates 'elin.handler.navigate/jump))))
+              (ix/when location-list?)
+              (ix/discard))})
+
+(def location-function-hook-interceptor
+  {:kind e.c.interceptor/quickfix
+   :params []
+   :enter (-> (fn [ctx]
+                (let [{:keys [params]} (e.u.interceptor/self ctx)]
+                  (assoc ctx ::params params)))
+              (ix/when location-list?))
+   :leave (-> (fn [{:component/keys [host] ::keys [params]}]
+                (when (and (seq params)
+                           (satisfies? e.p.rpc/IFunction host))
+                  (apply e.p.rpc/notify-function host params)))
+              (ix/when location-list?)
               (ix/discard))})
