1ec1c3cdc511f1e1f6572ca8a5b490115aadb859
liquidz <liquidz.uo@gmail.com>
2024-09-22 20:44:57 +0900

    feat: Add setting-nrepl-connection-status-interceptor

--- src/elin/interceptor/handler.clj
+++ src/elin/interceptor/handler.clj
@@ -3,6 +3,8 @@
    [elin.constant.interceptor :as e.c.interceptor]
    [elin.error :as e]
    [elin.message :as e.message]
+   [elin.protocol.host :as e.p.host]
+   [elin.util.interceptor :as e.u.interceptor]
    [exoscale.interceptor :as ix]))
 
 (def handling-error-interceptor
@@ -11,3 +13,23 @@
                 (e.message/error host (ex-message response)))
               (ix/when (comp e/error? :response))
               (ix/discard))})
+
+(def setting-nrepl-connection-status-interceptor
+  "Interceptor for setting nREPL connection status to variable."
+  (let [status-handler? #(= :elin.handler.internal/status
+                            (get-in % [:message :method]))]
+    {:kind e.c.interceptor/handler
+     :params [""]
+     :enter (-> (fn [ctx]
+                  (let [{:keys [params]} (e.u.interceptor/self ctx)
+                        variable-name (first params)]
+                    (assoc ctx ::variable-name variable-name)))
+                (ix/when status-handler?))
+     :leave (-> (fn [{:component/keys [host] ::keys [variable-name] :keys [response]}]
+                  (when (and (string? variable-name)
+                             (string? response)
+                             (seq variable-name)
+                             (seq response))
+                    (e.p.host/set-variable! host variable-name response)))
+                (ix/when status-handler?)
+                (ix/discard))}))
