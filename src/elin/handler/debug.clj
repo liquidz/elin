774e5c5387417dce0828e0c988030751481e1bcc
liquidz <liquidz.uo@gmail.com>
2024-10-05 16:26:50 +0900

    feat: Add elin.handler.debug

---
+++ src/elin/handler/debug.clj
@@ -0,0 +1,24 @@
+(ns elin.handler.debug
+  (:require
+   [clojure.core.async :as async]
+   [elin.error :as e]
+   [elin.protocol.host :as e.p.host]
+   [elin.protocol.nrepl :as e.p.nrepl]
+   [elin.util.nrepl :as e.u.nrepl]
+   [taoensso.timbre :as timbre]))
+
+(defn nrepl-request
+  [{:component/keys [host nrepl] :keys [message]}]
+  (when-let [request (try
+                       (some->> message
+                                (:params)
+                                (first)
+                                (read-string))
+                       (catch Exception ex
+                         (timbre/error "Invalid parameter" ex message)
+                         nil))]
+    (e/-> (e.p.nrepl/request nrepl request)
+          (async/<!!)
+          (e.u.nrepl/merge-messages)
+          (pr-str)
+          (->> (e.p.host/echo-message host)))))
