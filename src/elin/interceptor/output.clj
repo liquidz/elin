4e3eed17cfa5803efc808854753c9bfe90f46d44
liquidz <liquidz.uo@gmail.com>
2024-10-05 16:27:04 +0900

    fix: Update print-output-interceptor make it configurable

--- src/elin/interceptor/output.clj
+++ src/elin/interceptor/output.clj
@@ -2,10 +2,27 @@
   (:require
    [elin.constant.interceptor :as e.c.interceptor]
    [elin.protocol.host :as e.p.host]
+   [elin.util.interceptor :as e.u.interceptor]
+   [elin.util.string :as e.u.string]
    [exoscale.interceptor :as ix]))
 
 (def print-output-interceptor
+  "Interceptor to print output on nREPL to InfoBuffer.
+
+  Output format can be configured like below:
+  ```
+  {:interceptor {:config-map {elin.interceptor.output/print-output-interceptor
+                              {:format \"{{text}}\"}}}}
+  ```
+
+  Available variables:
+  - type: Output type
+  - text: Output text"
   {:kind e.c.interceptor/output
-   :enter (-> (fn [{:component/keys [host] :keys [output]}]
-                (e.p.host/append-to-info-buffer host (format ";; %s\n%s" (:type output) (:text output))))
+   :enter (-> (fn [{:as ctx :component/keys [host] :keys [output]}]
+                (let [config (e.u.interceptor/config ctx #'print-output-interceptor)
+                      format-str (or (:format config)
+                                     "{{text}}")]
+                  (->> (e.u.string/render format-str output)
+                       (e.p.host/append-to-info-buffer host))))
               (ix/discard))})
