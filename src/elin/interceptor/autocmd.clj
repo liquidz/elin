a967dd631e5a3219771ca1db501b531128b3a865
liquidz <liquidz.uo@gmail.com>
2024-10-06 16:44:24 +0900

    feat: Add elin.interceptor.autocmd/ns-load

--- src/elin/interceptor/autocmd.clj
+++ src/elin/interceptor/autocmd.clj
@@ -7,9 +7,12 @@
    [elin.function.nrepl :as e.f.nrepl]
    [elin.function.nrepl.namespace :as e.f.n.namespace]
    [elin.function.sexpr :as e.f.sexpr]
+   [elin.handler.evaluate :as e.h.evaluate]
    [elin.protocol.clj-kondo :as e.p.clj-kondo]
    [elin.protocol.host :as e.p.host]
    [elin.protocol.nrepl :as e.p.nrepl]
+   [elin.util.interceptor :as e.u.interceptor]
+   [elin.util.string :as e.u.string]
    [exoscale.interceptor :as ix]
    [taoensso.timbre :as timbre]))
 
@@ -23,22 +26,39 @@
               (ix/when #(= "VimLeave" (:autocmd-type %)))
               (ix/discard))})
 
+(defn- ns-not-created?
+  [{:component/keys [host nrepl]}]
+  (and (not (e.p.nrepl/disconnected? nrepl))
+       (nil? (async/<!! (e.p.host/get-variable! host ns-created-var-name)))))
+
+(defn- bufread-or-bufenter?
+  [{:keys [autocmd-type]}]
+  (contains? #{"BufRead" "BufEnter"} autocmd-type))
+
 (def ns-create
   {:kind e.c.interceptor/autocmd
-   :enter (-> (fn [{:as ctx :component/keys [host nrepl] :keys [autocmd-type]}]
-                (when (and (contains? #{"BufRead" "BufEnter"} autocmd-type)
-                           (not (e.p.nrepl/disconnected? nrepl))
-                           (nil? (async/<!! (e.p.host/get-variable! host ns-created-var-name))))
-                  (e/let [ns-str (e.f.sexpr/get-namespace ctx)
-                          ns-sym (or (symbol ns-str)
-                                     (e/incorrect))]
-                    (->> `(when-not (clojure.core/find-ns '~ns-sym)
-                            (clojure.core/create-ns '~ns-sym)
-                            (clojure.core/in-ns '~ns-sym)
-                            (clojure.core/refer-clojure))
-                         (str)
-                         (e.f.nrepl/eval!! nrepl))
-                    (async/<!! (e.p.host/set-variable! host ns-created-var-name true)))))
+   :enter (-> (fn [{:as ctx :component/keys [host nrepl]}]
+                (e/let [ns-str (e.f.sexpr/get-namespace ctx)
+                        ns-sym (or (symbol ns-str)
+                                   (e/incorrect))]
+                  (->> `(when-not (clojure.core/find-ns '~ns-sym)
+                          (clojure.core/create-ns '~ns-sym)
+                          (clojure.core/in-ns '~ns-sym)
+                          (clojure.core/refer-clojure))
+                       (str)
+                       (e.f.nrepl/eval!! nrepl))
+                  (async/<!! (e.p.host/set-variable! host ns-created-var-name true))))
+              (ix/when #(and (bufread-or-bufenter? %)
+                             (ns-not-created? %)))
+              (ix/discard))})
+
+(def ns-load
+  {:kind e.c.interceptor/autocmd
+   :enter (-> (fn [{:as ctx :component/keys [host]}]
+                (e.h.evaluate/evaluate-current-buffer ctx)
+                (e.p.host/set-variable! host ns-created-var-name true))
+              (ix/when #(and (bufread-or-bufenter? %)
+                             (ns-not-created? %)))
               (ix/discard))})
 
 (defmulti generate-skeleton
