1aaa431e75a1d17c4dd8eed95fb6667b7a5d48d2
liquidz <liquidz.uo@gmail.com>
2024-10-19 05:45:53 +0900

    style: Format protocols

--- src/elin/protocol/nrepl.clj
+++ src/elin/protocol/nrepl.clj
@@ -1,21 +1,37 @@
 (ns elin.protocol.nrepl)
 
 (defprotocol IConnection
-  (disconnect [this])
-  (disconnected? [this])
-  (notify [this msg])
-  (request [this msg]))
+  (disconnect
+    [this])
+  (disconnected?
+    [this])
+  (notify
+    [this msg])
+  (request
+    [this msg]))
 
 (defprotocol IClient
-  (supported-op? [this op])
-  (current-session [this])
-  (version [this]))
+  (supported-op?
+    [this op])
+  (current-session
+    [this])
+  (version
+    [this]))
 
 (defprotocol IClientManager
-  (add-client! [this host port] [this client])
-  (remove-client! [this client])
-  (remove-all! [this])
-  (get-client [this host port] [this client-key])
-  (switch-client! [this client])
-  (current-client [this])
-  (all-clients [this]))
+  (add-client!
+    [this host port]
+    [this client])
+  (remove-client!
+    [this client])
+  (remove-all!
+    [this])
+  (get-client
+    [this host port]
+    [this client-key])
+  (switch-client!
+    [this client])
+  (current-client
+    [this])
+  (all-clients
+    [this]))
