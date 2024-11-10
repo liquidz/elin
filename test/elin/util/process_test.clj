1ed23158015bb84a8905e6e8e78c9f594a3f8704
liquidz <liquidz.uo@gmail.com>
2024-11-10 15:20:31 +0900

    test: Fix elin.util.process tests

--- test/elin/util/process_test.clj
+++ test/elin/util/process_test.clj
@@ -3,27 +3,25 @@
    [clojure.test :as t]
    [elin.util.process :as sut]))
 
-(t/deftest start-test
+(t/deftest start-and-kill-test
   (t/testing "random process-id"
-    (let [id (sut/start ["echo" "'foo'"])]
+    (let [id (sut/start ["cat"])]
       (t/is (some? id))
-      (t/is (true? (sut/alive? id)))
       (Thread/sleep 100)
-      (t/is (false? (sut/alive? id)))))
+      (t/is (true? (sut/alive? id)))
+      (t/is (some? (sut/kill id)))
+
+      (Thread/sleep 100)
+      (t/is (false? (sut/alive? id)))
+      (t/is (nil? (sut/kill id)))))
 
   (t/testing "specified process-id"
     (let [id (random-uuid)]
-      (t/is (= id (sut/start id ["echo" "'foo'"])))
-      (t/is (true? (sut/alive? id)))
+      (t/is (= id (sut/start id ["cat"])))
       (Thread/sleep 100)
-      (t/is (false? (sut/alive? id))))))
+      (t/is (true? (sut/alive? id)))
+      (t/is (some? (sut/kill id)))
 
-(t/deftest kill-test
-  (let [id (sut/start ["cat"])]
-    (Thread/sleep 100)
-    (t/is (true? (sut/alive? id)))
-    (t/is (some? (sut/kill id)))
-
-    (Thread/sleep 100)
-    (t/is (false? (sut/alive? id)))
-    (t/is (nil? (sut/kill id)))))
+      (Thread/sleep 100)
+      (t/is (false? (sut/alive? id)))
+      (t/is (nil? (sut/kill id))))))
