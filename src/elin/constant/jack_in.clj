1e27bea9dffb3ed3779bf9be9fe8334d9f1a680a
liquidz <liquidz.uo@gmail.com>
2024-11-23 17:28:46 +0900

    feat: Add squint support for instant connecting

--- src/elin/constant/jack_in.clj
+++ src/elin/constant/jack_in.clj
@@ -3,5 +3,25 @@
 (def clojure-cli :clojure-cli)
 (def leiningen :leiningen)
 (def babashka :babashka)
+(def squint :squint)
 
-(def supported-project-types [clojure-cli babashka])
+(def supported-project-types
+  [clojure-cli
+   babashka
+   squint])
+
+(def clojure-command
+  (or (System/getenv "ELIN_REPL_CLOJURE_CLI_CMD")
+      "clj"))
+
+(def babashka-command
+  (or (System/getenv "ELIN_REPL_BABASHKA_CMD")
+      "bb"))
+
+(def squint-command
+  (or (System/getenv "ELIN_REPL_SQUINT_CMD")
+      "squint"))
+
+(def deno-command
+  (or (System/getenv "ELIN_REPL_DENO_CMD")
+      "deno"))
--- src/elin/function/jack_in.clj
+++ src/elin/function/jack_in.clj
@@ -4,18 +4,12 @@
    [clojure.edn :as edn]
    [clojure.java.io :as io]
    [elin.constant.jack-in :as e.c.jack-in]
+   [elin.constant.nrepl :as e.c.nrepl]
    [elin.error :as e]
    [elin.protocol.host :as e.p.host]
    [elin.util.nrepl :as e.u.nrepl]
-   [elin.util.process :as e.u.process]))
-
-(def ^:private clojure-command
-  (or (System/getenv "ELIN_REPL_CLOJURE_CLI_CMD")
-      "clj"))
-
-(def ^:private babashka-command
-  (or (System/getenv "ELIN_REPL_BABASHKA_CMD")
-      "bb"))
+   [elin.util.process :as e.u.process]
+   [taoensso.timbre :as timbre]))
 
 (def ^:private elin-root-dir
   (-> (io/file *file*)
@@ -67,26 +61,45 @@
       (edn/read-string)
       (get-in [:__elin_internal__ :command])))
 
-(defn generate-command
-  ([project-type port]
-   (generate-command project-type port []))
-  ([project-type port optional-args]
-   (condp = project-type
-     e.c.jack-in/clojure-cli
-     (concat [clojure-command]
-             optional-args
-             ["-Sdeps" (pr-str {:deps (:deps command-config)})
-              "-M" "-m" "nrepl.cmdline"
-              "--port" (str port)
-              "--middleware" (pr-str (:middlewares command-config))
-              "--interactive"])
+(defmulti generate-command
+  (fn [project-type _port _optional-args]
+    project-type))
 
-     e.c.jack-in/babashka
-     [babashka-command
-      "nrepl-server"
-      (str "localhost:" port)]
+(defmethod generate-command :default
+  [_ _ _]
+  (e/unsupported))
 
-     (e/unsupported))))
+(defmethod generate-command e.c.jack-in/clojure-cli
+  [_ port optional-args]
+  {:language e.c.nrepl/lang-clojure
+   :command (concat [e.c.jack-in/clojure-command]
+              optional-args
+              ["-Sdeps" (pr-str {:deps (:deps command-config)})
+               "-M" "-m" "nrepl.cmdline"
+               "--port" (str port)
+               "--middleware" (pr-str (:middlewares command-config))
+               "--interactive"])})
+
+(defmethod generate-command e.c.jack-in/babashka
+  [_ port _]
+  {:language e.c.nrepl/lang-clojure
+   :command [e.c.jack-in/babashka-command
+             "nrepl-server"
+             (str "localhost:" port)]})
+
+(defmethod generate-command e.c.jack-in/squint
+  [_ port _]
+  (e/let [squint-cmd (cond
+                       (e.u.process/executable? e.c.jack-in/squint-command)
+                       [e.c.jack-in/squint-command]
+
+                       (e.u.process/executable? e.c.jack-in/deno-command)
+                       [e.c.jack-in/deno-command "run" "-A" "npm:squint-cljs@latest"]
+
+                       :else
+                       (e/not-found {:message "squint of deno command is not found"}))]
+    {:language e.c.nrepl/lang-clojurescript
+     :command (concat squint-cmd ["nrepl-server" ":port" (str port)])}))
 
 (defn port->process-id
   [port]
@@ -101,7 +114,7 @@
            [project-type project-file] (select-project options path)
            project-root-dir (parent-absolute-path project-file)
            port (e.u.nrepl/get-free-port)
-           args (->> (generate-command project-type port)
-                     (cons {:dir project-root-dir}))]
+           {:keys [language command]} (generate-command project-type port [])
+           args (cons {:dir project-root-dir} command)]
      (e.u.process/start (port->process-id port) args)
-     port)))
+     {:language language :port port})))
