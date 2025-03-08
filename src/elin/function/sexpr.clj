6e98ea06ab90b1a9c659e146fe9a3bdd34342990
liquidz <liquidz.uo@gmail.com>
2025-03-08 11:55:43 +0900

    fix: Fix error handling in sexpr functions

--- src/elin/function/sexpr.clj
+++ src/elin/function/sexpr.clj
@@ -9,41 +9,56 @@
    [elin.util.sexpr :as e.u.sexpr]
    [malli.core :as m]))
 
+(m/=> validate-code-and-position [:=> [:cat e.s.host/?CodeAndPosition] (e.schema/error-or e.s.host/?CodeAndPosition)])
+(defn- validate-code-and-position
+  [{:as code-and-position :keys [code]}]
+  (if (seq code)
+    code-and-position
+    (e/not-found)))
+
 (m/=> get-top-list [:function
                     [:-> e.s.handler/?Elin int? int? (e.schema/error-or e.s.host/?CodeAndPosition)]
                     [:-> e.s.handler/?Elin string? int? int? (e.schema/error-or e.s.host/?CodeAndPosition)]])
 (defn get-top-list
   ([{:component/keys [host]} lnum col]
-   (async/<!! (e.p.host/get-top-list-sexpr! host lnum col)))
+   (e/-> (async/<!! (e.p.host/get-top-list-sexpr! host lnum col))
+         (validate-code-and-position)))
   ([{:component/keys [host]} path lnum col]
-   (async/<!! (e.p.host/get-top-list-sexpr! host path lnum col))))
+   (e/-> (async/<!! (e.p.host/get-top-list-sexpr! host path lnum col))
+         (validate-code-and-position))))
 
 (m/=> get-list [:function
                 [:-> e.s.handler/?Elin int? int? (e.schema/error-or e.s.host/?CodeAndPosition)]
                 [:-> e.s.handler/?Elin string? int? int? (e.schema/error-or e.s.host/?CodeAndPosition)]])
 (defn get-list
   ([{:component/keys [host]} lnum col]
-   (async/<!! (e.p.host/get-list-sexpr! host lnum col)))
+   (e/-> (async/<!! (e.p.host/get-list-sexpr! host lnum col))
+         (validate-code-and-position)))
   ([{:component/keys [host]} path lnum col]
-   (async/<!! (e.p.host/get-list-sexpr! host path lnum col))))
+   (e/-> (async/<!! (e.p.host/get-list-sexpr! host path lnum col))
+         (validate-code-and-position))))
 
 (m/=> get-expr [:function
                 [:-> e.s.handler/?Elin int? int? (e.schema/error-or e.s.host/?CodeAndPosition)]
                 [:-> e.s.handler/?Elin string? int? int? (e.schema/error-or e.s.host/?CodeAndPosition)]])
 (defn get-expr
   ([{:component/keys [host]} lnum col]
-   (async/<!! (e.p.host/get-single-sexpr! host lnum col)))
+   (e/-> (async/<!! (e.p.host/get-single-sexpr! host lnum col))
+         (validate-code-and-position)))
   ([{:component/keys [host]} path lnum col]
-   (async/<!! (e.p.host/get-single-sexpr! host path lnum col))))
+   (e/-> (async/<!! (e.p.host/get-single-sexpr! host path lnum col))
+         (validate-code-and-position))))
 
 (m/=> get-namespace-sexpr [:function
                            [:=> [:cat e.s.handler/?Elin] (e.schema/error-or e.s.host/?CodeAndPosition)]
                            [:=> [:cat e.s.handler/?Elin string?] (e.schema/error-or e.s.host/?CodeAndPosition)]])
 (defn get-namespace-sexpr
   ([{:component/keys [host]}]
-   (async/<!! (e.p.host/get-namespace-sexpr! host)))
+   (e/-> (async/<!! (e.p.host/get-namespace-sexpr! host))
+         (validate-code-and-position)))
   ([{:component/keys [host]} path]
-   (async/<!! (e.p.host/get-namespace-sexpr! host path))))
+   (e/-> (async/<!! (e.p.host/get-namespace-sexpr! host path))
+         (validate-code-and-position))))
 
 (m/=> replace-list-sexpr [:=> [:cat e.s.handler/?Elin int? int? string?] (e.schema/error-or [:maybe string?])])
 (defn replace-list-sexpr
