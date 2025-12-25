605a0d6cd12ab328891c9cf0b2c44d083de72b14
liquidz <liquidz.uo@gmail.com>
2025-12-25 14:53:49 +0900

    fix: Add default port and host for dev server

--- dev/elin/task/nrepl.clj
+++ dev/elin/task/nrepl.clj
@@ -5,12 +5,16 @@
    [bencode.core :as b]
    [elin.dev :as e.dev]))
 
+(def DEFAULT_NREPL_SERVER_PORT 34567)
+(def DEFAULT_DEV_SERVER_HOST "nvim")
+(def DEFAULT_DEV_SERVER_PORT 45678)
+
 (defn- start-dev-server!
   [{:keys [port options]}]
   (with-open [s (java.net.Socket. "localhost" port)
               output-stream (.getOutputStream s)]
-    (let [host (or (:host options) "nvim")
-          port (or (:port options) 45678)]
+    (let [host (or (:host options) DEFAULT_DEV_SERVER_HOST)
+          port (or (:port options) DEFAULT_DEV_SERVER_PORT)]
       (b/write-bencode output-stream
                        {"op" "eval"
                         "code" (str `(do (elin.dev/initialize {:host ~host :port ~port})
@@ -18,11 +22,11 @@
 
 (defn -main
   [& [options]]
-  (let [{:keys [socket]} (srv/start-server! {:host "localhost" :port 0})
+  (let [{:keys [socket]} (srv/start-server! {:host "localhost" :port DEFAULT_NREPL_SERVER_PORT})
         port (.getLocalPort socket)]
     (spit ".nrepl-port" (str port))
     (-> (Runtime/getRuntime)
         (.addShutdownHook
-         (Thread. (fn [] (fs/delete ".nrepl-port")))))
+          (Thread. (fn [] (fs/delete ".nrepl-port")))))
     (start-dev-server! {:port port :options options})
     (deref (promise))))
