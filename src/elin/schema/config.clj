67c980360c03afa0529036177481533cb772e83a
liquidz <liquidz.uo@gmail.com>
2025-12-31 13:45:25 +0900

    feat: Add ?ConfigLinter schema to config.clj

--- src/elin/schema/config.clj
+++ src/elin/schema/config.clj
@@ -1,13 +1,25 @@
-(ns elin.schema.config)
+(ns elin.schema.config
+  (:require
+   [malli.util :as mu]))
 
 (def ^:private ?Env
   [:map
    [:cwd string?]])
 
+(def ^:private ?ConfigMap
+  [:map-of qualified-symbol? map?])
+
+(def ^:private ?HandlerAlias
+  [:map
+   [:handler qualified-symbol?]
+   [:config map?]])
+
 (def ^:private ?Handler
   [:map
    [:includes {:default []} [:sequential qualified-symbol?]]
-   [:excludes {:default []} [:sequential qualified-symbol?]]])
+   [:excludes {:default []} [:sequential qualified-symbol?]]
+   [:config-map {:default {}} ?ConfigMap]
+   [:aliases {:default {}} [:map-of symbol? ?HandlerAlias]]])
 
 (def ?InterceptorItem
   [:or
@@ -17,19 +29,22 @@
 (def ^:private ?Interceptor
   [:map
    [:includes {:default []} [:sequential ?InterceptorItem]]
-   [:excludes {:default []} [:sequential ?InterceptorItem]]])
+   [:excludes {:default []} [:sequential ?InterceptorItem]]
+   [:config-map {:default {}} ?ConfigMap]])
 
 (def ^:private ?LogLevel
   [:enum :debug :info :warning :error])
 
 (def ^:private ?Log
   [:map
-   [:level {:default :info} ?LogLevel]])
+   [:min-level {:default :info} ?LogLevel]
+   [:appenders {:optional true} map?]])
 
 (def ^:private ?Server
   [:map
    [:host string?]
-   [:port int?]])
+   [:port int?]
+   [:entrypoints map?]])
 
 (def ?Config
   [:map
@@ -38,3 +53,13 @@
    [:interceptor {:default {}} ?Interceptor]
    [:log {:default {}} ?Log]
    [:server ?Server]])
+
+(def ?ConfigLinter
+  (let [top-schema-keys (->>
+                          (mu/subschemas ?Config)
+                          (filter #(= 1 (count (:path %))))
+                          (map (comp first :path)))]
+    (reduce
+      (fn [accm k]
+        (mu/update accm k mu/closed-schema))
+      ?Config top-schema-keys)))
